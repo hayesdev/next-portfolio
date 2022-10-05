171af01b683e5d237188514de6dc85eb1d206539
Greg Hayes <gthelight@gmail.com>
2022-10-05 17:58:32 -0600

    adds framer motion lib

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,73 @@
-import React from 'react'
-import {SocialIcon} from "react-social-icons"
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 function Header({}: Props) {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between mx-auto max-w-7xl z-20 xl:items-center'>
-      <div className='flex flex-row items-center'>
-      {/* social icons */}
-      <SocialIcon url="https://www.youtube.com" fgColor='gray' bgColor='transparent'/>
-      <SocialIcon url="https://www.youtube.com" fgColor='gray' bgColor='transparent'/>
-      <SocialIcon url="https://www.youtube.com" fgColor='gray' bgColor='transparent'/>
-       </div>
-      <div className="flex flex-row cursor-pointer items-center text-gray-300">
-        <SocialIcon className="cursor-pointer" network="email" fgColor='gray' bgColor='transparent'/>
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch!</p>
-      </div>
+    <header className="sticky top-0 p-5 flex items-start justify-between mx-auto max-w-7xl z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        {/* social icons */}
+        <SocialIcon
+          url="https://www.youtube.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row cursor-pointer items-center text-gray-300"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch!
+        </p>
+      </motion.div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
