5dc221f4104770f00b33250a82e4cc6b8b87a340
Greg Hayes <gthelight@gmail.com>
2022-10-10 19:26:26 -0600

    adds about component

---
+++ components/About.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl mx-auto px-10 justify-evenly items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        src="../gorski-rpg.png"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 object-cover rounded-full md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+      />
+      <div className="space-y-10 px-0 py-4 md:px-10">
+        <h4 className="text-2xl md:text-4xl font-semibold">
+          Here's a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+          about me...
+        </h4>
+        <p className="text-base">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+          aperiam ipsam illum sit rerum dolorum quos sint, ipsa temporibus odio
+          laudantium, eos cum tenetur fugiat molestiae cupiditate ex dolore
+          provident! Iusto nam nemo fugit provident adipisci consectetur
+          reprehenderit itaque nobis modi dolorem, eligendi eaque accusamus rem
+          cumque amet sit fugiat.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
