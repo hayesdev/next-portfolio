2743846c8700e668ccf8fbcb70c23fd0103c84ce
Greg Hayes <gthelight@gmail.com>
2022-10-15 12:41:18 -0600

    fixes warning in projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,7 +21,11 @@ function Projects({}: Props) {
       {/* Projects */}
       <div className="relative w-full flex overflow-scroll-x overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project, index) => (
-          <div className="w-screen h-screen flex-shrink-0 snap-center flex flex-col space-y-5 justify-center items-center p-20 md:p-44 ">
+          <div
+            // Children in a list should have a unique key
+            key={index}
+            className="w-screen h-screen flex-shrink-0 snap-center flex flex-col space-y-5 justify-center items-center p-20 md:p-44 "
+          >
             <motion.img
               initial={{ y: -300, opacity: 0 }}
               transition={{ duration: 1.2 }}
