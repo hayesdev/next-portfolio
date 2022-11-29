d667df903dbba55d3d97fd4832232829a655039c
Greg Hayes <gthelight@gmail.com>
2022-11-28 22:47:55 -0500

    fix for skill animations

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -10,12 +10,12 @@ function Skill({ directionLeft }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
-        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
-        transition={{ duration: 1 }}
+        initial={{ x: directionLeft ? -80 : 80, opacity: 0 }}
+        transition={{ duration: 1.25 }}
         whileInView={{ opacity: 1, x: 0 }}
         src="../react.png"
         // grayscale effect on hover
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in"
+        className="rounded-full border border-gray-500 object-cover w-16 h-16 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in"
       />
     </div>
   );
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,7 +10,7 @@ function Skills({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="min-h-screen flex relative text-center md:text-left xl:flex-row max-w-[2000px] mx-auto xl:px-10 xl:space-y-0 justify-center items-center"
+      className="min-h-screen flex relative text-center md:text-left xl:flex-row max-w-[2000px] min-w-[375px] mx-auto xl:px-10 xl:space-y-0 justify-center items-center"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
@@ -19,15 +19,15 @@ function Skills({}: Props) {
         Hover over for more information
       </h3>
       <div className="grid grid-cols-4 gap-5">
+        <Skill directionLeft={true} />
+        <Skill directionLeft={true} />
+        <Skill directionLeft={true} />
         <Skill />
         <Skill />
         <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <Skill directionLeft={true} />
+        <Skill directionLeft={true} />
+        <Skill directionLeft={true} />
         <Skill />
         <Skill />
         <Skill />
